7600463a73aa38381f44cc529abf8acac52dc03d
santoshki <santoshkdevp@gmail.com>
2026-09-05 12:25:05 +0530

    1.Updated snow_workflow.yml pipeline file with stages for artifact upload and finalize. 2.Updated main.tf to delete the existing users from SNOW instance.

--- modules/user/main.tf
+++ modules/user/main.tf
@@ -1,8 +1,8 @@
-resource "servicenow_user" "this" {
-  user_name  = var.user_name
-  first_name = var.first_name
-  last_name  = var.last_name
-  email      = var.email
-  title      = var.title
-  active     = var.active
-}
+# resource "servicenow_user" "this" {
+#   user_name  = var.user_name
+#   first_name = var.first_name
+#   last_name  = var.last_name
+#   email      = var.email
+#   title      = var.title
+#   active     = var.active
+# }
