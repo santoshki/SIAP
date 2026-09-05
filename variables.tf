4e8089a4b61055da5330f91433c93a14e2108ff3
santoshki <santoshkdevp@gmail.com>
2026-09-05 12:33:29 +0530

    1.Updated outputs.tf and variables.tf code for deletion of exiting users created using tf code.

--- variables.tf
+++ variables.tf
@@ -1,28 +1,28 @@
-variable "servicenow_users" {
-  description = "ServiceNow users to create"
-
-  type = map(object({
-    user_name  = string
-    first_name = string
-    last_name  = string
-    email      = string
-    title      = string
-    active     = bool
-  }))
-}
-variable "servicenow_instance_url" {
-  description = "ServiceNow instance URL"
-  type        = string
-}
-
-variable "servicenow_username" {
-  description = "ServiceNow username"
-  type        = string
-  sensitive   = true
-}
-
-variable "servicenow_password" {
-  description = "ServiceNow password"
-  type        = string
-  sensitive   = true
-}
+# variable "servicenow_users" {
+#   description = "ServiceNow users to create"
+#
+#   type = map(object({
+#     user_name  = string
+#     first_name = string
+#     last_name  = string
+#     email      = string
+#     title      = string
+#     active     = bool
+#   }))
+# }
+# variable "servicenow_instance_url" {
+#   description = "ServiceNow instance URL"
+#   type        = string
+# }
+#
+# variable "servicenow_username" {
+#   description = "ServiceNow username"
+#   type        = string
+#   sensitive   = true
+# }
+#
+# variable "servicenow_password" {
+#   description = "ServiceNow password"
+#   type        = string
+#   sensitive   = true
+# }
